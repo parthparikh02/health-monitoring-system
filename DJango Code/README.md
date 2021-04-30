f69d736e74626b4a2bff5229373a053919f48e65
Parth Parikh <36477389+parthparikh02@users.noreply.github.com>
2021-04-30 18:25:35 +0530

    Delete DJango Code directory

--- DJango Code/README.md
+++
@@ -1,2 +0,0 @@
-# healthmonitoringsystem
-This is Repo. is about my final year project on Patient Health Monitoring System using IOT.
--- DJango Code/healthmonitoring/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin1/', admin.site.urls),
-    path('', include('new.urls')),
-]
